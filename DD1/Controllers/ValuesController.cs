a7d4339cedf1cf20bd0dd661b1effc64203e6da2
dandan53 <dandan53@gmail.com>
2015-08-10 17:36:20 +0300

    Init
    Adding project

---
+++ DD1/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+
+namespace DD1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            try
+            {
+                string fromEmail = "dandan53@gmail.com";
+                MailMessage mailMessage = new MailMessage(fromEmail, "dandan53@gmail.com", "Subject", "Body");
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(fromEmail, "haim1234");
+                
+                smtpClient.Send(mailMessage);
+                
+                //MailMessage mail = new MailMessage();
+                //SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                //mail.From = new MailAddress("dandan53@gmail.com");
+                //mail.To.Add("dandan53@gmail.com");
+                //mail.Subject = "Test Mail";
+                //mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                //SmtpServer.Port = 587;
+                //SmtpServer.Credentials = new System.Net.NetworkCredential("user", "pass");
+                //SmtpServer.EnableSsl = true;
+
+                //SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                return new string[] { "error" };
+            }
+            
+            
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DD1/Scripts/app.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module("DDApp", ['ngRoute']).
+    config(function ($routeProvider) {
+        $routeProvider.
+            when('/', { controller: MainCtrl, templateUrl: 'main.html' }).
+            otherwise({ redirectTo: '/' });
+    });
+
+//TodoApp.controller(ListCtrl);
+
+var MainCtrl = function ($scope, $location) {
+    $scope.form = {};
+   // $scope.form.name = "dan daniel";
+   // $scope.form.phone = "052-1234561";
+   
+    $scope.title = "dan daniel";
+
+    $scope.send = function() {
+        alert($scope.form.name);
+    };
+};
