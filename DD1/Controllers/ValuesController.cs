d20646a9e28a3af825fcb60bd5d2912f7b53fc3c
dandan53 <dandan53@gmail.com>
2015-08-12 14:51:24 +0300

    sendMsg - mail

--- DD1/Controllers/ValuesController.cs
+++ DD1/Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
 using System.Web.Http;
 
 namespace DD1.Controllers
@@ -12,8 +15,59 @@ namespace DD1.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
+            SendByEmail("subject", "messageBody","");
+
             return new string[] { "value1", "value2" };
         }
+
+        public void SendByEmail(string subject, string messageBody, string recipientsGroup)
+        {
+            string addresses = ConfigurationManager.AppSettings[recipientsGroup];
+
+            string errStr = "";
+
+            try
+            {
+                
+
+                var msg = new MailMessage
+                {
+                    From = new MailAddress("chenvardi9@gmail.com", "Deposit Alert"),
+                    Subject = subject,
+                    Body = messageBody
+                };
+
+                //if (!string.IsNullOrWhiteSpace(addresses))
+                //{
+                //    string[] csvList = addresses.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                //    foreach (string address in csvList)
+                //    {
+                //        if (!string.IsNullOrWhiteSpace(addresses))
+                //        {
+                //            msg.To.Add(new MailAddress(address));
+                //        }
+                //    }
+
+                msg.To.Add(new MailAddress("dandan53@gmail.com"));
+
+                if (msg.To.Count > 0)
+                {
+                    var SMTP = new SmtpClient();
+                    SMTP.EnableSsl = true;
+                    SMTP.Send(msg);
+                }
+            }
+                //}
+            catch (Exception ex)
+            {
+                errStr = ex.ToString();
+
+            }
+
+            string ee = errStr + "";
+        }
+    
 
         // GET api/values/5
         public string Get(int id)
--- DD1/Scripts/app.js
+++ DD1/Scripts/app.js
@@ -7,7 +7,7 @@
 
 //TodoApp.controller(ListCtrl);
 
-var MainCtrl = function ($scope, $location) {
+var MainCtrl = function ($scope, $location, contactService) {
     $scope.form = {};
    // $scope.form.name = "dan daniel";
    // $scope.form.phone = "052-1234561";
@@ -15,6 +15,106 @@ var MainCtrl = function ($scope, $location) {
     $scope.title = "dan daniel";
 
     $scope.send = function() {
-        alert($scope.form.name);
+     //   alert($scope.form.name);
+        sendMsg();
     };
+    
+
+    ///// http ////
+    
+    $scope.sendMsg = function () {
+
+        contactService.sendMsg($scope.bid_id)
+                        .then(
+                            loadData,
+                            function (errorMessage) {
+
+                                console.warn(errorMessage);
+
+                            }
+                        )
+        ;
+    };
+
+    function loadData(data) {
+        // $scope.item = data;
+        alert(data);
+    };
+
+
+    $scope.sendMsg();
 };
+
+
+app.service("contactService", function ($http, $q) {
+
+    // Return public API.
+    return ({
+        sendMsg: sendMsg
+    });
+
+    // ---
+    // PUBLIC METHODS.
+    // ---
+
+    function sendMsg(id) {
+
+        var request = $http({
+            method: "get",
+            url: "/api/values/",
+            params: {
+                //id: id
+            }
+        });
+
+        return (request.then(handleSuccess, handleError));
+
+    }
+
+
+  
+
+    // ---
+    // PRIVATE METHODS.
+    // ---
+
+
+    // I transform the error response, unwrapping the application dta from
+    // the API response payload.
+    function handleError(response) {
+
+        // The API response from the server should be returned in a
+        // nomralized format. However, if the request was not handled by the
+        // server (or what not handles properly - ex. server error), then we
+        // may have to normalize it on our end, as best we can.
+        if (
+            !angular.isObject(response.data) ||
+            !response.data.message
+            ) {
+
+            return ($q.reject("An unknown error occurred."));
+
+        }
+
+        alert("handleError");
+
+        // Otherwise, use expected error message.
+        return ($q.reject(response.data.message));
+
+
+    }
+
+
+    // I transform the successful response, unwrapping the application data
+    // from the API response payload.
+    function handleSuccess(response) {
+
+        // alert("handleSuccess");
+
+        return (response.data);
+
+    }
+
+}
+);
+
